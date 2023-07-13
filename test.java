6dcca94e90b717e4002f9861bac1dfb970b3c3ea
Johanrva <129812828+Johanrva@users.noreply.github.com>
2023-07-13 17:59:24 -0500

    Test installing java

---
+++ test.java
@@ -0,0 +1,7 @@
+// Para poder correr java instalamos JDK enla maquina y las extensiones necesarias para visual-studio-code
+
+class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
