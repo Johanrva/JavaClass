d578bdaa6f1b0249d4f64b8ec0041c0d6fab503d
Johanrva <129812828+Johanrva@users.noreply.github.com>
2023-07-18 14:00:23 -0500

    Controller and Services of Doctor and Appointment, class 17/06

--- doctores/src/main/kotlin/com/doctores/doctores/constants/Routes.kt
+++ doctores/src/main/kotlin/com/doctores/doctores/constants/Routes.kt
@@ -8,18 +8,18 @@ const val Appointment = "$ApiV1/appointments"
 const val Patient = "$ApiV1/patients"
 
 // Doctors
-const val CreateDoctors = "/$Doctor/create"
-const val GetDoctorById = "/$Doctor/{id}"
-const val UpdateDoctor = "/$Doctor/{id}"
+const val CreateDoctors = "$Doctor/create"
+const val GetDoctorById = "$Doctor/{id}"
+const val UpdateDoctor = "$Doctor/{id}"
 
 // Appointment
-const val CreateAppointments = "/$Appointment/create"
-const val GetAppointmentById = "/$Appointment/{id}"
-const val UpdateAppointment = "/$Appointment/{id}"
+const val CreateAppointments = "$Appointment/create"
+const val GetAppointmentById = "$Appointment/{id}"
+const val UpdateAppointment = "$Appointment/{id}"
 
 // Patients
-const val CreatePatients = "/$Patient/create"
-const val GetPatientById = "/$Patient/{id}"
-const val UpdatePatient = "/$Patient/{id}"
+const val CreatePatients = "$Patient/create"
+const val GetPatientById = "$Patient/{id}"
+const val UpdatePatient = "$Patient/{id}"
 
 
